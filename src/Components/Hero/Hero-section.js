57bdf9cf89316602141ee9da65159791e6911bd9
Calin Radu <calinradualexandru@protonmail.com>
2021-02-15 16:06:20 +0200

    logo animation @media

--- src/Components/Hero/Hero-section.js
+++ src/Components/Hero/Hero-section.js
@@ -36,74 +36,74 @@ const HeroSection = () => {
       <HeroBg>
         <ImgBg alt="programming setup" src={laptop} />
       </HeroBg>
-      <div class="animation01">
-        <div class="rhombus_small">
-          <div class="rhombus">
-            <div class="border_box">
-              <span class="line line01"></span>
-              <span class="line line02"></span>
-              <span class="line line03"></span>
-              <span class="line line04"></span>
-              <span class="circle circle01"></span>
-              <span class="circle circle02"></span>
-              <span class="circle circle03"></span>
-              <span class="circle circle04"></span>
-              <span class="animation_line animation_line01"></span>
-              <span class="animation_line_wrapper animation_line02_wrapper">
-                <span class="animation_line animation_line02"></span>
+      <div className="animation01">
+        <div className="rhombus_small">
+          <div className="rhombus">
+            <div className="border_box">
+              <span className="line line01"></span>
+              <span className="line line02"></span>
+              <span className="line line03"></span>
+              <span className="line line04"></span>
+              <span className="circle circle01"></span>
+              <span className="circle circle02"></span>
+              <span className="circle circle03"></span>
+              <span className="circle circle04"></span>
+              <span className="animation_line animation_line01"></span>
+              <span className="animation_line_wrapper animation_line02_wrapper">
+                <span className="animation_line animation_line02"></span>
               </span>
-              <span class="animation_line animation_line03"></span>
-              <span class="animation_line_wrapper animation_line04_wrapper">
-                <span class="animation_line animation_line04"></span>
+              <span className="animation_line animation_line03"></span>
+              <span className="animation_line_wrapper animation_line04_wrapper">
+                <span className="animation_line animation_line04"></span>
               </span>
-              <span class="animation_line animation_line05"></span>
-              <span class="animation_line_wrapper animation_line06_wrapper">
-                <span class="animation_line animation_line06"></span>
+              <span className="animation_line animation_line05"></span>
+              <span className="animation_line_wrapper animation_line06_wrapper">
+                <span className="animation_line animation_line06"></span>
               </span>
-              <span class="animation_line animation_line07"></span>
-              <span class="animation_line_wrapper animation_line08_wrapper">
-                <span class="animation_line animation_line08"></span>
+              <span className="animation_line animation_line07"></span>
+              <span className="animation_line_wrapper animation_line08_wrapper">
+                <span className="animation_line animation_line08"></span>
               </span>
             </div>
-            <div class="wave">
-              <div class="wave_wrapper">
-                <div class="wave_box"></div>
+            <div className="wave">
+              <div className="wave_wrapper">
+                <div className="wave_box"></div>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div class="animation02">
-        <div class="rhombus_box">
-          <span class="rhombus_item_wrapper rhombus_item01_wrapper">
-            <span class="rhombus_item"></span>
+      <div className="animation02">
+        <div className="rhombus_box">
+          <span className="rhombus_item_wrapper rhombus_item01_wrapper">
+            <span className="rhombus_item"></span>
           </span>
-          <span class="rhombus_item_wrapper rhombus_item02_wrapper">
-            <span class="rhombus_item"></span>
+          <span className="rhombus_item_wrapper rhombus_item02_wrapper">
+            <span className="rhombus_item"></span>
           </span>
         </div>
-        <div class="double_content">
-          <div class="double_wrapper02 dotted02">
-            <div class="dotted_hide">
-              <div class="double_wrapper01 dotted01">
-                <span class="dotted_right"></span>
+        <div className="double_content">
+          <div className="double_wrapper02 dotted02">
+            <div className="dotted_hide">
+              <div className="double_wrapper01 dotted01">
+                <span className="dotted_right"></span>
               </div>
             </div>
           </div>
-          <div class="double_wrapper02 white02">
-            <div class="double_wrapper01 white01"></div>
+          <div className="double_wrapper02 white02">
+            <div className="double_wrapper01 white01"></div>
           </div>
-          <div class="double_wrapper02 gray02">
-            <div class="double_wrapper01 gray01"></div>
+          <div className="double_wrapper02 gray02">
+            <div className="double_wrapper01 gray01"></div>
           </div>
-          <div class="double_wrapper02 orange02">
-            <div class="double_wrapper01 orange01"></div>
+          <div className="double_wrapper02 orange02">
+            <div className="double_wrapper01 orange01"></div>
           </div>
         </div>
-        <div class="name">
+        <div className="name">
           <p></p>
-          <span class="name_circle01"></span>
-          <span class="name_circle02"></span>
+          <span className="name_circle01"></span>
+          <span className="name_circle02"></span>
         </div>
       </div>
       {background && (
