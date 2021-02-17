fddd02f0e5e08d92a92ef74c1b0d6780b329d8c4
Calin Radu <calinradualexandru@protonmail.com>
2021-02-17 17:50:47 +0200

    hero button arrow

--- src/Components/Hero/Hero-elements.js
+++ src/Components/Hero/Hero-elements.js
@@ -230,9 +230,25 @@ export const HeroButton = styled(LinkS)`
 export const ArrowForward = styled(MdArrowForward)`
   margin-left: 8px;
   font-size: 20px;
+
+  @media screen and (max-width: 800px) {
+    font-size: 20px;
+  }
+
+  @media screen and (max-width: 300px) {
+    font-size: 20px;
+  }
 `;
 
 export const ArrowRight = styled(MdKeyboardArrowRight)`
   margin-left: 8px;
   font-size: 20px;
+
+  @media screen and (max-width: 800px) {
+    font-size: 20px;
+  }
+
+  @media screen and (max-width: 300px) {
+    font-size: 20px;
+  }
 `;
